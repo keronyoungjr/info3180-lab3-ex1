d67498a24159a4afb21b1d621a114fb952f1352c
keronyoungjr <keronyoungjr@yahoo.com>
2017-02-08 17:02:05 +0000

    first commit

---
+++ sendemail.py
@@ -0,0 +1,24 @@
+import smtplib
+
+def sendemail(from_name, from_addr, to_name,to_addr, subject, msg):
+    from_addr = 'keronthelevite@gmail.com'
+    from_name= "Keron"
+    to_name= 'Keron'
+    to_addr = 'keronyoungjr@yahoo.com'
+    Subject= 'Lab 3  Email Test'
+    message = 'Mad thing. You received an email. Great.'
+    
+     
+    
+    message_to_send = message.format(from_name, from_addr, to_name,to_addr, subject, msg)
+    # Credentials (if needed)
+    
+    username = 'keronthelevite@gmail.com'
+    password = '{uklpvbtyfwwftikx}'
+    
+    # The actual mail send
+    server = smtplib.SMTP('smtp.gmail.com:587')
+    server.starttls()
+    server.login(username, password)
+    server.sendmail(from_addr, to_addr, message_to_send)
+    server.quit() 
